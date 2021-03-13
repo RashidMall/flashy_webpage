9ec117ae3d80db27645899fb52cc72ae1f1a58b8
RashidMall <mr.mallaev@gmail.com>
2021-03-13 22:32:26 +0100

    FEATURE: Add animated typed text

--- js/main.js
+++ js/main.js
@@ -4,4 +4,13 @@ jQuery(document).ready(function() {
         dropRadius: 13,
         perturbance: 0.01,
     });
+
+    var typed = new Typed('.text', {
+        strings: [
+            "<span class='weight-bold'>While there's</span> <span class='weight-bold primary'>code.</span>",
+            "<span class='weight-bold'>there's</span> <span class='weight-bold primary'>bug!</span>"
+        ],
+        typeSpeed: 60,
+        loop: true
+    });
 });
